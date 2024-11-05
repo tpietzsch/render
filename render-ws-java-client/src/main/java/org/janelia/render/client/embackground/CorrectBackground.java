7c93790801918a3478570df1361c100a65e753e7
Michael Innerberger <innerbergerm@hhmi.org>
2024-11-05 17:42:30 -0500

    Fit model from first slice

--- render-ws-java-client/src/main/java/org/janelia/render/client/embackground/CorrectBackground.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/embackground/CorrectBackground.java
@@ -1,6 +1,11 @@
 package org.janelia.render.client.embackground;
 
 import ij.ImageJ;
+import mpicbg.models.IllDefinedDataPointsException;
+import mpicbg.models.NotEnoughDataPointsException;
+import mpicbg.models.Point;
+import mpicbg.models.PointMatch;
+import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.converter.Converters;
@@ -14,12 +19,15 @@ import org.janelia.saalfeldlab.n5.N5FSReader;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CorrectBackground {
 	private static final String containerPath = System.getenv("HOME") + "/big-data/render-exports/cerebellum-3.n5";
 	private static final String dataset = "data";
 	private static final int scale = 4;
 
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws NotEnoughDataPointsException, IllDefinedDataPointsException {
 		try (final N5Reader reader = new N5FSReader(containerPath)) {
 			final Img<UnsignedByteType> stack = N5Utils.open(reader, dataset + "/s" + scale);
 			final RandomAccessibleInterval<UnsignedByteType> firstSlice = Views.hyperSlice(stack, 2, 0);
@@ -30,12 +38,31 @@ public class CorrectBackground {
 			final int midX = width / 2;
 			final int midY = height / 2;
 
-			final RealRandomAccessible<FloatType> background = new FunctionRealRandomAccessible<>(2, (pos, value) -> {
-				final double x = (pos.getDoublePosition(0) - midX) / width;
-				final double y = (pos.getDoublePosition(1) - midY) / height;
-				value.setReal(1 - (x * x + y * y) / 2.0);
-			}, FloatType::new);
+			// fit a quadratic background model with all the points in the first slice
+			final QuadraticBackground backgroundModel = new QuadraticBackground();
+			final List<PointMatch> matches = new ArrayList<>();
+			final Cursor<UnsignedByteType> cursor = Views.iterable(firstSlice).localizingCursor();
+			while (cursor.hasNext()) {
+				cursor.fwd();
+				final double x = (cursor.getDoublePosition(0) - midX) / width;
+				final double y = (cursor.getDoublePosition(1) - midY) / height;
+				final double z = cursor.get().getRealDouble();
+				matches.add(new PointMatch(new Point(new double[]{x, y}), new Point(new double[]{z})));
+			}
+			backgroundModel.fit(matches);
+			System.out.println("Fitted background model: " + backgroundModel);
 
+			// we assume that the model is concave, so the offset is the maximum value
+			final double maxValue = backgroundModel.getCoefficients()[5];
+
+			// create a background image from the model
+			final double[] location = new double[2];
+			final RealRandomAccessible<FloatType> background = new FunctionRealRandomAccessible<>(2, (pos, value) -> {
+				location[0] = (pos.getDoublePosition(0) - midX) / width;
+				location[1] = (pos.getDoublePosition(1) - midY) / height;
+				backgroundModel.applyInPlace(location);
+				value.setReal(location[0] / maxValue);
+			}, FloatType::new);
 
 			final RandomAccessibleInterval<FloatType> materializedBackground = Views.interval(Views.raster(background), firstSlice);
 
@@ -45,6 +72,7 @@ public class CorrectBackground {
 
 			new ImageJ();
 			ImageJFunctions.show(firstSlice, "Original");
+			ImageJFunctions.show(materializedBackground, "Background");
 			ImageJFunctions.show(corrected, "Corrected");
 		}
 	}
