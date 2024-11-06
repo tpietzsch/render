863bc3943b6fea72641e718aa29cd9bd626791ca
Michael Innerberger <innerbergerm@hhmi.org>
2024-11-06 18:40:08 -0500

    Make plugin actually compute background

--- render-ws-java-client/src/main/java/org/janelia/render/client/embackground/BG_Plugin.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/embackground/BG_Plugin.java
@@ -1,12 +1,18 @@
 package org.janelia.render.client.embackground;
 
 import java.awt.FlowLayout;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import mpicbg.models.IllDefinedDataPointsException;
+import mpicbg.models.NotEnoughDataPointsException;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.real.FloatType;
 import org.janelia.saalfeldlab.n5.N5FSReader;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
@@ -25,42 +31,71 @@ public class BG_Plugin implements PlugIn {
 	public static int defaultType = 0;
 	public static String[] fitTypes = new String[] { "Quadratic", "Fourth Order" };
 
+	public static RandomAccessibleInterval<UnsignedByteType> img;
+
 	@Override
 	public void run(final String arg) {
-		final Roi rois = getROIs();
+		final List<Roi> rois = getROIs();
 
-		if (rois == null) {
+		if (rois == null || rois.isEmpty()) {
+			IJ.log("No ROIs specified.");
 			return;
 		}
 
-		final GenericDialog gd = new GenericDialog("fit...");
+		final GenericDialog gd = new GenericDialog("Fit background correction");
 
-		gd.addChoice("fit_type", fitTypes, fitTypes[defaultType]);
+		gd.addChoice("Fit type", fitTypes, fitTypes[defaultType]);
 		gd.showDialog();
 
 		if (gd.wasCanceled()) {
 			return;
 		}
 
-		defaultType = gd.getNextChoiceIndex();
 		final int type = gd.getNextChoiceIndex();
+		defaultType = type;
 
-		fit(type);
+		try {
+			fit(type, rois);
+		} catch (final NotEnoughDataPointsException | IllDefinedDataPointsException e) {
+			IJ.log("Fitting failed: " + e.getMessage());
+		}
 	}
 
-	public static Roi getROIs() {
+	public static List<Roi> getROIs() {
 		final RoiManager rm = RoiManager.getInstance();
 
 		if (rm == null || rm.getCount() == 0) {
-			IJ.log("please define rois ... ");
+			IJ.log("Please define ROIs first before running background correction.");
 			return null;
 		}
 
-		return rm.getRoi( 0 );
+		return Arrays.asList(rm.getRoisAsArray());
 	}
 
-	public static void fit(final int type) {
-		IJ.log("fitting with ... " + fitTypes[type]);
+	public static void fit(final int type, final List<Roi> rois) throws NotEnoughDataPointsException, IllDefinedDataPointsException {
+		IJ.log("Fitting with " + fitTypes[type] + " model...");
+
+		final BackgroundModel<?> backgroundModel;
+		if (fitTypes[type].equals("Quadratic")) {
+			backgroundModel = new QuadraticBackground();
+		} else if (fitTypes[type].equals("Fourth Order")) {
+			backgroundModel = new FourthOrderBackground();
+		} else {
+			throw new IllegalArgumentException("Unknown fit type: " + fitTypes[type]);
+		}
+
+		final long start = System.currentTimeMillis();
+		CorrectBackground.fitBackgroundModel(rois, img, backgroundModel);
+		IJ.log("Fitted background model: " + backgroundModel);
+		IJ.log("Fitting took " + (System.currentTimeMillis() - start) + "ms.");
+		IJ.log("Raw coefficients: " + Arrays.toString(backgroundModel.getCoefficients()));
+
+		final RandomAccessibleInterval<FloatType> background = CorrectBackground.createBackgroundImage(backgroundModel, img);
+		final RandomAccessibleInterval<UnsignedByteType> corrected = CorrectBackground.correctBackground(img, background, new UnsignedByteType());
+
+		ImageJFunctions.show(background, "Background");
+		ImageJFunctions.show(corrected, "Corrected");
+
 	}
 
 	private static void showNonBlockingDialog() {
@@ -77,14 +112,13 @@ public class BG_Plugin implements PlugIn {
         }).start();
 	}
 
-	@SuppressWarnings({"unchecked", "rawtypes"})
 	public static void main(final String[] args) {
 		new ImageJ();
 
 		SwingUtilities.invokeLater(BG_Plugin::showNonBlockingDialog);
 
 		final String n5Path = System.getenv("HOME") + "/big-data/render-exports/cerebellum-3.n5";
-		RandomAccessibleInterval img = N5Utils.open(new N5FSReader(n5Path), "data/s4");
+		img = N5Utils.open(new N5FSReader(n5Path), "data/s4");
 		final int z = 0;
 		img = Views.hyperSlice(img, 2, z);
 		ImageJFunctions.show(img);
