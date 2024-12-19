fbbd08f06b2cce835f10a239797ada402bccb268
tpietzsch <tobias.pietzsch@gmail.com>
2024-12-19 16:21:50 +0100

    Speed up LinearIntensityMap8BitFilter
    Use FastLinearIntensityMap.
    
    For now only when the input is ByteProcessor.
    FastLinearIntensityMap should work the same for other types.
    If necessary just add if branches with wrappers for the other
    ImageProcessor variants...

--- render-app/src/main/java/org/janelia/alignment/filter/LinearIntensityMap8BitFilter.java
+++ render-app/src/main/java/org/janelia/alignment/filter/LinearIntensityMap8BitFilter.java
@@ -2,15 +2,21 @@ package org.janelia.alignment.filter;
 
 import ij.ImagePlus;
 import ij.ImageStack;
+import ij.process.ByteProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 
+import net.imglib2.algorithm.blocks.BlockSupplier;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import org.janelia.alignment.filter.FastLinearIntensityMap.Coefficients;
 import org.janelia.alignment.intensity.LinearIntensityMap;
 
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.imageplus.ImagePlusImgs;
 import net.imglib2.type.numeric.real.FloatType;
+
+import static org.janelia.alignment.filter.FastLinearIntensityMap.linearIntensityMap;
 
 public class LinearIntensityMap8BitFilter
         extends IntensityMap8BitFilter {
@@ -59,40 +65,50 @@ public class LinearIntensityMap8BitFilter
     public void process(final ImageProcessor ip,
                         final double scale) {
 
-        final FloatProcessor as = new FloatProcessor(numberOfRegionColumns, numberOfRegionRows);
-        final FloatProcessor bs = new FloatProcessor(numberOfRegionColumns, numberOfRegionRows);
+        if (ip instanceof ByteProcessor) {
+            final byte[] pixels = (byte[]) ip.getPixels();
+            final Img<UnsignedByteType> img = ArrayImgs.unsignedBytes(pixels, ip.getWidth(), ip.getHeight());
+            final Coefficients c = new Coefficients(coefficients, numberOfRegionColumns, numberOfRegionRows );
+            BlockSupplier.of(img)
+                    .andThen(linearIntensityMap(c, img))
+                    .tile(256)
+                    .copy(img, pixels);
+        } else {
+            final FloatProcessor as = new FloatProcessor(numberOfRegionColumns, numberOfRegionRows);
+            final FloatProcessor bs = new FloatProcessor(numberOfRegionColumns, numberOfRegionRows);
 
-        // this filter always produces 8-bit corrected output
-        final FloatProcessor fp = ip.convertToFloatProcessor();
-        fp.resetMinAndMax();
-        final double min = 0;
-        final double max = 255;
+            // this filter always produces 8-bit corrected output
+            final FloatProcessor fp = ip.convertToFloatProcessor();
+            fp.resetMinAndMax();
+            final double min = 0;
+            final double max = 255;
 
-        for (int i = 0; i < coefficients.length; ++i) {
-            final double[] ab = coefficients[i];
+            for (int i = 0; i < coefficients.length; ++i) {
+                final double[] ab = coefficients[i];
 
-            /* coefficients mapping into existing [min, max] */
-            as.setf(i, (float) ab[0]);
-            bs.setf(i, (float) ((max - min) * ab[1] + min - ab[0] * min));
+                /* coefficients mapping into existing [min, max] */
+                as.setf(i, (float) ab[0]);
+                bs.setf(i, (float) ((max - min) * ab[1] + min - ab[0] * min));
+            }
+            final ImageStack coefficientsStack = new ImageStack(numberOfRegionColumns, numberOfRegionRows);
+            coefficientsStack.addSlice(as);
+            coefficientsStack.addSlice(bs);
+
+            final LinearIntensityMap<FloatType> map =
+                    new LinearIntensityMap<FloatType>(
+                            ImagePlusImgs.from(
+                                    new ImagePlus("", coefficientsStack)
+                            )
+                    );
+
+            final long[] dims = new long[]{ip.getWidth(), ip.getHeight()};
+            final Img<FloatType> img = ArrayImgs.floats((float[]) fp.getPixels(), dims);
+
+            map.run(img);
+
+            // Need to reset intensity range back to full 8-bit before converting to byte processor!
+            fp.setMinAndMax(0, 255);
+            ip.setPixels(0, fp);
         }
-        final ImageStack coefficientsStack = new ImageStack(numberOfRegionColumns, numberOfRegionRows);
-        coefficientsStack.addSlice(as);
-        coefficientsStack.addSlice(bs);
-
-        final LinearIntensityMap<FloatType> map =
-                new LinearIntensityMap<FloatType>(
-                        ImagePlusImgs.from(
-                                new ImagePlus("", coefficientsStack)
-                        )
-                );
-
-        final long[] dims = new long[]{ip.getWidth(), ip.getHeight()};
-        final Img<FloatType> img = ArrayImgs.floats((float[]) fp.getPixels(), dims);
-
-        map.run(img);
-
-        // Need to reset intensity range back to full 8-bit before converting to byte processor!
-        fp.setMinAndMax(0, 255);
-        ip.setPixels(0, fp);
     }
 }
